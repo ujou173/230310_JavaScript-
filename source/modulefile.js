01607cb1a77a3ebbda2da57eb6fa4dd4839f5d1f
sumin <ujou173@gmail.com>
2023-03-10 14:17:20 +0900

    기록 시작

---
+++ source/modulefile.js
@@ -0,0 +1,67 @@
+const _EXAMDATA = {
+  teamInformation: {
+    teamNumber : 3,
+    teamName: "아이즈원",
+    teamNameStory: "MBTI가 I인 5명이 하나로 뭉쳤다!",
+    teamManager: "류은이",
+    teamAllMember: [
+      {
+        memberNumber: 1,
+        name: "류은이",
+        teamPosition: "팀장",
+        emailAddress: "euni.ryu97@gmail.com",
+        githubAddress: "https://github.com/RyuEuni",
+        word: "이제 같은 팀으로써 얘기도 많이 나누고 마주칠텐데 친하게 지내요! 그리고 제가 아는게 많이 없는데 잘 부탁드립니다",
+      },
+      {
+        memberNumber: 2,
+        name: "김종윤",
+        teamPosition: "팀원",
+        emailAddress: "kimsilveryoon96@gmail.com",
+        githubAddress: "https://github.com/kimjongyoon96",
+        word: "부족하지만 잘 부탁 드립니다.",
+      },
+      {
+        memberNumber: 3,
+        name: "김지섭",
+        teamPosition: "팀원",
+        emailAddress: "jsub5646@gmail.com",
+        githubAddress: "https://github.com/JseopKim",
+        word: "첫번째 프로젝트인 만큼 재미있게 했으면 좋겠습니다.",
+      },
+      {
+        memberNumber: 4,
+        name: "김형진",
+        teamPosition: "팀원",
+        emailAddress: "bigbowltakestime@gmail.com",
+        githubAddress: "https://github.com/bigbowltakestime",
+        word: "열심히 하겠습니다 잘 부탁드려요",
+      },
+      {
+        memberNumber: 5,
+        name: "노수민",
+        teamPosition: "팀원",
+        emailAddress: "rosumin173@gmail.com",
+        githubAddress: "https://github.com/ujou173",
+        word: "우리 다같이 합을 맞춰서 열심히 공부하고 교류하면서 서로 힘내봅시다! 아이즈원, 화이팅!",
+      },
+    ]
+  },
+  trainingInformation: {
+    subject : [  
+    "웹프로그래밍기본(HTML5&CSS3)",
+    "Git&GitHub",
+    "Babel Javascript",
+    "프레임워크 기초",
+    "Project C(선택)",
+    "백엔드프레임워크런타임",
+    "프레임워크활용",
+    "Project A(데이터 기반 주식 추천 서비스)",
+    "Project B(교통정보제공서비스)",
+    ]
+  },
+  designInformation: {
+    colorSet : ["cadetblue", "salmon", "#ccc", "#333"],
+    breakPoint : [ "768px", "1280px",  "1920px"]
+  }
+}
